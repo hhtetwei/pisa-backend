32d06e8044a673544d85c1f0eaedd78eb005739b
Grace Wei <hhtet263510@gmail.com>
2025-07-20 21:00:49 +0700

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://pisa-frontend-xvuu.vercel.app/'],
   credentials: true
 }));
 
